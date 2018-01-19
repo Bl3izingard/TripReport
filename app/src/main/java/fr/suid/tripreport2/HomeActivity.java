9ea2c2a67e22b08686b7389983d193be5b50d741
Bl3izingard <jpierre@suid.fr>
2018-01-19 11:28:00 +0100

    Commit #10

--- app/src/main/java/fr/suid/tripreport2/HomeActivity.java
+++ app/src/main/java/fr/suid/tripreport2/HomeActivity.java
@@ -26,8 +26,9 @@ public class HomeActivity extends AppCompatActivity
 			@Override
 			public void onClick(View view)
 			{
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				Intent i = new Intent(HomeActivity.this, MouvementFormActivity.class);
+
+				startActivityForResult(i, 1);
 			}
 		});
 	}
