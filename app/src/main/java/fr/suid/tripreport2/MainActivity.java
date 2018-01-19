2411fd7d70cbfbb23a71b0bd0d17886611c382ae
Bl3izingard <jpierre@suid.fr>
2018-01-19 10:42:04 +0100

    Initial Commit

--- app/src/main/java/fr/suid/tripreport2/MainActivity.java
+++ app/src/main/java/fr/suid/tripreport2/MainActivity.java
@@ -1,15 +1,34 @@
 package fr.suid.tripreport2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity
 {
+
+	private static int TripReport_TIME_OUT = 2000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		//Passage à la vue suivante pas de retour possible
+		Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+				startActivity(homeIntent);
+				finish();
+			}
+		}, TripReport_TIME_OUT);
 	}
+
+
 }
